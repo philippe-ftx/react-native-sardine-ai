e8d3876c14f177234f41ff8ccf658a465d21adc6
Taylor Johnson <cjohns28@terpmail.umd.edu>
2022-01-26 11:42:28 -0500

    catch android setup error (#2)
    * try app context
    
    * catch throwable instead of just exception
    
    * bump version

--- android/src/main/java/com/taylor123/sardineai/MobileIntelligenceModule.java
+++ android/src/main/java/com/taylor123/sardineai/MobileIntelligenceModule.java
@@ -46,9 +46,9 @@ public class MobileIntelligenceModule extends ReactContextBaseJavaModule {
         }
         
         try {
-            MobileIntelligence.init(getCurrentActivity(), optionBuilder.build());
+            MobileIntelligence.init(this.getReactApplicationContext(), optionBuilder.build());
             promise.resolve(new Response(true, "Initialized successfully").getGson());
-        } catch (Exception e) {
+        } catch (Throwable e) {
             promise.reject(e);
         }
     }
